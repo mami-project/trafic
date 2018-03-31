7d9053056517f03b2263d807ee4d616fd802f9e8
Thomas Fossati <thomas.fossati@nokia.com>
2018-03-31 16:24:31 +0100

    fix misplaced comments

--- cruncher/cruncher.go
+++ cruncher/cruncher.go
@@ -5,14 +5,13 @@ import (
 )
 
 type Cruncher interface {
-	// A Cruncher can take an iperf3 JSON report (either UDP or TCP) and
-	// transform it into a format that works for a certain consumer (e.g.,
-	// Telegraf, InfluxDB, CSV, etc.)
+	// A Cruncher takes an iperf3 JSON report (either UDP or TCP) and
+	// transforms it into a format that can be natively consumed by a certain
+	// storage/processing engine (e.g., Telegraf, InfluxDB, CSV, etc.)
 	CrunchUDP(UDPFlowStats) ([]byte, error)
 	CrunchTCP(TCPFlowStats) ([]byte, error)
 }
 
-// Telegraf-friendly representation
 func Crunch(c Cruncher, j []byte) ([]byte, error) {
 	var tcpFlowStats TCPFlowStats
 	var udpFlowStats UDPFlowStats
