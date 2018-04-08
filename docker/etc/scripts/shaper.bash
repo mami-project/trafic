b0085e1af00146a3285587b2672e8d9529dd2527
Thomas Fossati <thomas.fossati@nokia.com>
2018-04-08 19:30:47 +0100

    add loss

--- docker/etc/scripts/shaper.bash
+++ docker/etc/scripts/shaper.bash
@@ -1,13 +1,14 @@
 #!/bin/bash
 #
 # LTE (UE<->SGi LAN)
-# - UL [iperf-server]: MAXRATE=50Mbit  LATENCY=92ms MTU=1500 IFACE=eth0
-# - DL [iperf-client]: MAXRATE=100Mbit LATENCY=76ms MTU=1500
+# - UL [iperf-server]: MAXRATE=50Mbit  LATENCY=92ms MTU=1500 IFACE=eth0 PKTLOSS=0.025%
+# - DL [iperf-client]: MAXRATE=100Mbit LATENCY=76ms MTU=1500 PKTLOSS=0.025%
 
 set -eu
 
 IFACE=${IFACE:?"set IFACE to the network interface you want to shape, e.g.: eth0"}
 LATENCY=${LATENCY:?"set LATENCY to the upstream network latency, e.g.: 50ms"}
+PKTLOSS=${PKTLOSS:?"set PKTLOSS to the percentage of packet lost on the upstream link, e.g.: 0.1%"}
 MAXRATE=${MAXRATE:?"set MAXRATE to the maximum upstream throughput rate, e.g.: 40Mbit"}
 MTU=${MTU:?"set MTU to the maximum transfer unit, e.g.: 1500"}
 
@@ -20,7 +21,7 @@ echo ">> Setting upstream latency to ${LATENCY} and max throughput rate to ${MAX
 tc qdisc add dev ${IFACE} handle 1: root htb default 11
 tc class add dev ${IFACE} parent 1: classid 1:1 htb rate ${MAXRATE} ceil ${MAXRATE}
 tc class add dev ${IFACE} parent 1:1 classid 1:11 htb rate ${MAXRATE} ceil ${MAXRATE}
-tc qdisc add dev ${IFACE} parent 1:11 handle 10: netem delay ${LATENCY}
+tc qdisc add dev ${IFACE} parent 1:11 handle 10: netem delay ${LATENCY} loss ${PKTLOSS}
 
 # show current configuration
 echo ">> Shaping settings on ${IFACE}:"
