7b62fab258b97bad43d589643d56f6033eb53ceb
Thomas Fossati <thomas.fossati@nokia.com>
2018-05-04 15:20:40 +0100

    per-test DB and stats folder

--- docker/etc/scripts/run-clients.bash
+++ docker/etc/scripts/run-clients.bash
@@ -2,15 +2,26 @@
 
 set -exu
 
+function mklabel() {
+	local exid=$1
+	local unixtime=$(date +%s)
+
+	printf "lola-%s-%s" "${exid}" "${unixtime}"
+}
+
+EXID=${EXID:-vanilla}
 CONF=${CONF:-flows.env}
 
 base=$(dirname $0)
 . ${base}/${CONF}
 
+label=$(mklabel "$EXID")
+STATS=${STATS:-/root/share/stats/$label}
+
 schedule clients \
-	--stats-dir=/root/share/stats \
+	--stats-dir="${STATS}" \
 	--log-tag=C \
 	--flows-dirs=${FLOWS} \
 	--influxdb-enabled \
 	--influxdb-endpoint=http://influxdb:8086 \
-	--influxdb-db=lola
+	--influxdb-db=${label}
