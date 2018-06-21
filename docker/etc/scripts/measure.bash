a662e03f32bbf774d896ecf9ad7d47f778dc9d6e
Pedro A. Aranda <paranda@it.uc3m.es>
2018-06-21 12:33:45 +0200

    An alternative script to control measurements remotely

--- docker/etc/scripts/measure.bash
+++ docker/etc/scripts/measure.bash
@@ -2,26 +2,29 @@
 
 set -eu
 
-function mk-label() {
+function mklabel() {
 	local exid=$1
 	local unixtime=$(date +%s)
 
 	printf "lola-%s-%s" "${exid}" "${unixtime}"
 }
 
-IFACE=${IFACE:-eth0}
+# remote measurements don't include capturing traffic
+#IFACE=${IFACE:-eth0}
+IFACE=
 EXID=${EXID:-baseline}
+HOST=${HOST:-iperf}
 
 for load in 75 80 85 90 95
 do
 	exid="${EXID}-${load}"
-	label=$(mk-label "${exid}")
-	# capfn="${label}.pcap"
+	label=$(mklabel "${exid}")
+	capfn="${label}.pcap"
 
 	# start servers
 	wget --header "X-CONF: ${exid}.env" \
 		-O /dev/null \
-		http://iperf-server:9000/hooks/start-servers
+		http://${HOST}-server:9000/hooks/start-servers
 
 	sleep 1
 
@@ -30,14 +33,19 @@ do
 		--header "X-LABEL: ${label}" \
 		--header "X-DB: ${EXID}" \
 		-O /dev/null \
-		http://iperf-client:9000/hooks/start-clients
+		http://${HOST}-client:9000/hooks/start-clients
 
-	sleep 65	# allow some time for flows to drain
-
+	if [ -n "${IFACE}" ]; then
+		# start capture for 60s
+		tshark -i ${IFACE} -s 128 -w ${capfn} -f 'tcp or udp' -a duration:60
+		# try to save as much space as possible
+		bzip2 -9 ${capfn}
+		sleep 5	# allow some time for flows to drain
+	else
+		sleep 65
+	fi
 	# cleanup (and, possibly, go again)
-	wget http://iperf-server:9000/hooks/stop-servers -O /dev/null
-	wget http://iperf-client:9000/hooks/stop-clients -O /dev/null
-
-	# send measurements to the postprocessing machine and clean up
-	wget http://iperf-client:9000/hooks/send-stats -O /dev/null
+	wget http://${HOST}-server:9000/hooks/stop-servers -O /dev/null
+	wget http://${HOST}-client:9000/hooks/stop-clients -O /dev/null
+	sleep 5
 done
