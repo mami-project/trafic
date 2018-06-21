96969abcc939586507d2f57d3764f811579507a4
Pedro A. Aranda <paranda@it.uc3m.es>
2018-06-21 12:25:04 +0200

    Included some extra hooks in the client to check

--- docker/etc/scripts/run-clients-stats.bash
+++ docker/etc/scripts/run-clients-stats.bash
@@ -9,7 +9,10 @@ DB=${DB:-lola}
 base=$(dirname $0)
 . ${base}/${CONF}
 
+STATS=${STATS:-/root/share/stats/$LABEL}
+
 schedule clients \
+	--stats-dir=${STATS} \
 	--log-tag=C \
 	--flows-dirs=${FLOWS} \
 	--influxdb-enabled \
