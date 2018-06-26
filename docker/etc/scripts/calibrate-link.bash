a2540a19aa9bab3a0da110402a2f7196a47a24df
root <paaguti@gmail.com>
2018-06-26 17:09:43 +0200

    Clean up check link and calibration

--- docker/etc/scripts/calibrate-link.bash
+++ docker/etc/scripts/calibrate-link.bash
@@ -4,10 +4,9 @@ set -eu
 
 function mk-label() {
 	local exid=$1
-	local ext=$2
 	local unixtime=$(date +%s)
 
-	printf "lola-%s-%s%s" "${exid}" "${unixtime}" "${ext}"
+	printf "lola-%s-%s" "${exid}" "${unixtime}"
 }
 cwd=`dirname $0`
 iperf3 -c iperf-server --reverse --time 30 --logfile ${cwd}/$(mk-label "calibrate").txt
