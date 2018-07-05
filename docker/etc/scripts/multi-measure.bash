0ddea98a34a4e4babb9e0212248ff80223524299
Pedro A. Aranda <paranda@it.uc3m.es>
2018-07-05 11:16:23 +0200

    Add iterations and gap

--- docker/etc/scripts/multi-measure.bash
+++ docker/etc/scripts/multi-measure.bash
@@ -1,16 +1,23 @@
 #!/bin/bash
-
 set -eum
 
+#
+# help: multi-measure.bash [iterations [gap]]
+#   default:  
+#      iterations=10
+#      gap=10
+
 function main() {
-	local i, max
+	local i, max, sleep
 
 	cwd=$(dirname $0)
 	i=0
 	max=${1:-10}
+	sleep=${2:-10}
 	while [ $i -lt $max ]; do
 		i=$((i + 1))
 		IFACE=eth0 ${cwd}/measure.bash ${i}
+		sleep ${sleep}
 	done
 }
 
