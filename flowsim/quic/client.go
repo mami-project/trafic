b00d697b3df22d630f12c4acb611f06670a40086
Pedro A. Aranda <paranda@it.uc3m.es>
2018-10-19 10:17:59 +0200

    Revert back to use the official quic-go by default

--- flowsim/quic/client.go
+++ flowsim/quic/client.go
@@ -28,7 +28,7 @@ func Client(ip string, port int, iter int, interval int, bunch int) error {
 	if err != nil {
 		return err
 	}
-	defer session.Close(err)
+	defer session.Close()
 	fmt.Printf("Opened session for %s\n", addr)
 	buf := make([]byte, bunch)
 	stream, err := session.OpenStreamSync()
