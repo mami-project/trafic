9dc9afb948e119abbc8c657e9f79d161432024a9
paaguti <paaguti@gmail.com>
2018-12-19 11:09:31 +0100

    Clean up and calc loss correctly

--- flowsim/udp/sink.go
+++ flowsim/udp/sink.go
@@ -7,36 +7,21 @@ import (
 
 func Sink(ip string, port int,verbose bool) {
 	fmt.Printf("Starting UDP sink at %s:%d\n", ip, port)
-    /* Lets prepare a address at any address at port 10001*/
+
     ServerAddr,err := net.ResolveUDPAddr("udp",fmt.Sprintf("%s:%d", ip, port))
     CheckError(err)
-
-    /* Now listen at selected port */
-	ServerConn, err := net.ListenUDP("udp", ServerAddr)
+	Conn, err := net.ListenUDP("udp", ServerAddr)
 	CheckError(err)
-	ServeOneConnection(ServerConn, verbose)
-}
-
-func ServeOneConnection(Conn *net.UDPConn, verbose bool) {
 	defer Conn.Close()
 
-	buf := make([]byte, 64 * 1024)
+	buf      := make([]byte, 64 * 1024)
+	// mdelay   := int64(0)
+	// mpackets := int64(0)
+	stats    := &Stats { 0,0,0, 0,0,0 }
 
-	var info myStruct
-	tStamp := MakeTimestamp()
-	// var last int64
-	// last = 0
-	// loss := 0
-	// ldelay   := int64(0)
-	mdelay   := int64(0)
-	mpackets := int64(0)
-	// mjitter  := int64(0)
-	stats    := NewStats()
-
-	// var fromUDP *net.UDPAddr
 	for {
 		n,fromUDP,err := Conn.ReadFromUDP(buf)
-		tStamp = MakeTimestamp()
+		tStamp := MakeTimestamp()
 		if verbose {
 			fmt.Println("Received ",n, "bytes from ",fromUDP)
 		}
@@ -44,14 +29,14 @@ func ServeOneConnection(Conn *net.UDPConn, verbose bool) {
 			fmt.Println("Error: ",err)
 			continue
 		}
-		info = DecodePacket(buf[0:n])
+		info := DecodePacket(buf[0:n])
 		udelay := tStamp - info.tStamp
 		AddSample(stats, int(udelay), int(info.pktId))
 		if verbose {
 			fmt.Printf("Delay was: %d us\n", udelay)
 		}
-		mdelay += udelay
-		mpackets ++
+		// mdelay += udelay
+		// mpackets ++
 
 		if (info.pktId == info.total) {
 			_,_,err := Conn.ReadFromUDP(buf) // discard last resort
