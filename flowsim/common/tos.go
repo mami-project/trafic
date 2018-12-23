ad12b1d16fe0be8f8e7a393d35f6c3b2dcb43e08
paaguti <paaguti@gmail.com>
2018-12-23 09:04:25 +0100

    Simplify ipv4/ipv6 call

--- flowsim/common/tos.go
+++ flowsim/common/tos.go
@@ -13,23 +13,23 @@ func SetTos(f *os.File, tos int,ipv6 bool) (error) {
 	// This needs to go in the caller code
 	// f, err := Conn.File()
 	// if err != nil {
-	// fmt.Printf("While setting TOS to %d on %v: %v\n", tos, f, err)
+	//   fmt.Printf("While setting TOS to %d on %v: %v\n", tos, f, err)
 	//   return err
 	// }
 
+	proto  := syscall.IPPROTO_IP
+	call   := syscall.IP_TOS
+	errmsg := "TOS"
 
-  if ipv6 {
-	err := syscall.SetsockoptInt(int(f.Fd()), syscall.IPPROTO_IPV6, syscall.IPV6_TCLASS, tos)
-	if err != nil {
-		fmt.Printf("While setting TCLASS to %d: %v\n", tos, err)
-	}
-	  return err
-  }
-
-	err := syscall.SetsockoptInt(int(f.Fd()), syscall.IPPROTO_IP, syscall.IP_TOS, tos)
-	if err != nil {
-		fmt.Printf("While setting TOS to %d: %v\n", tos, err)
+	if ipv6 {
+		proto  = syscall.IPPROTO_IPV6
+		call   = syscall.IPV6_TCLASS
+		errmsg = "TCLASS"
 	}
 
-  return err
+	err := syscall.SetsockoptInt(int(f.Fd()), proto, call, tos)
+	if err != nil {
+		fmt.Printf("While setting %s to %d: %v\n", errmsg, tos, err)
+	}
+	return err
 }
